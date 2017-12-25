e21bc3b7b4149fbecad79bcebd7875704c23c166
DK\ili <ili@sitecore.net>
2017-12-25 14:07:52 +0200

    Release

---
+++ src/Sitecore.Support.146550/Modules/EmailCampaign/Cm/Pipelines/SendEmail/CheckAndFixLinks.cs
@@ -0,0 +1,58 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Modules.EmailCampaign.Core.Extensions;
+using Sitecore.Modules.EmailCampaign.Core.Pipelines.GenerateLink;
+using System;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Web;
+using System.Text.RegularExpressions;
+using Sitecore.Modules.EmailCampaign.Core.Pipelines;
+using Sitecore.EDS.Core.Dispatch;
+
+
+namespace Sitecore.Support.EmailCampaign.Cm.Pipelines.SendEmail
+{
+  public class CheckIfLinkCorrect
+  {
+    public void Process(SendMessageArgs args)
+    {
+      Assert.IsNotNull(args, "Arguments can't be null");
+      string regexPattern = @"<a [^\s]*>";
+      EmailMessage emailMessage = args.CustomData["EmailMessage"] as EmailMessage;
+      Regex regex = new Regex(regexPattern);
+      Match match = regex.Match(emailMessage.HtmlBody);
+      string result = String.Empty;
+      string editResult = String.Empty;
+      while (match.Success)
+      {
+        result = match.Value;
+        if (!result.Contains("href"))
+        {
+          if (result.IndexOf("http") > -1)
+          {
+            Regex reg2 = new Regex(@"http[^\s]*\s");
+            if (reg2.IsMatch(result))
+            {
+              Match match2 = reg2.Match(@"http[^\s]*\s");
+              editResult = result.Replace(match2.Value, "href=\"" + match2.Value + "\"");
+              emailMessage.HtmlBody = emailMessage.HtmlBody.Replace(result, editResult);
+            }
+            else
+            {
+              Regex reg3 = new Regex(@"http[^\s]*>");
+              Match match3 = reg3.Match(result);
+              if (match3.Success)
+              {
+                string temp = match3.Value.Remove(match3.Value.IndexOf('>'));
+                editResult = result.Replace(temp, "href=\"" + temp + "\"");
+                emailMessage.HtmlBody = emailMessage.HtmlBody.Replace(result, editResult);
+              }
+            }
+          }
+        }
+        args.CustomData["EmailMessage"] = emailMessage;
+        match = match.NextMatch();
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.146550/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.146550")]
+[assembly: AssemblyProduct("Sitecore.Support.146550")]
+[assembly: ComVisible(false)]
